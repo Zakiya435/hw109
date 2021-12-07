4c8708882750242f6a736b8dfcd7091d288c52b3
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2021-12-07 20:42:00 +0530

    Add files via upload

---
+++ hw109.py
@@ -0,0 +1,29 @@
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+import plotly.graph_objects as go
+import statistics
+
+df = pd.read_csv("data_2.csv")
+data = df["reading score"].tolist()
+
+mean = statistics.mean(data)
+std_deviation = statistics.stdev(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+
+list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
+list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
+list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
+
+print("Mean: ",mean)
+print("Median: ",median)
+print("Mode: ",mode)
+print("Standard deviation: ",std_deviation)
+print("{}% of data lies within 1 standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 2 standard deviations".format(len(list_of_data_within_2_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 3 standard deviations".format(len(list_of_data_within_3_std_deviation)*100.0/len(data)))
